3c5c5c6d59bc82eb5f8e029e39a47db9a6a9f516
truthmyson <nageteychristopher@gmail.com>
2025-04-18 18:47:55 +0100

    Revert "add web app"
    This reverts commit 4661569c0931da00989f74a7af3db23294d9c714.

--- web_note/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(noteId) {
-  fetch("/delete_note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
